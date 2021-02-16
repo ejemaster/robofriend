4bc4bef42e0b2d97fad110b2d582e112fad1b699
ejemaster <ejemaster@gmx.de>
2021-02-16 11:24:12 +0100

    puhs gh-pages

---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+//import {robots} from './robots';
+import CardList from '../component/CardList'
+import SearchBox from '../component/SearchBox';
+import Scroll from '../component/Scroll';
+import './App.css'
+
+class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+          robots: [],
+          searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+      
+    }
+
+onSearchChange = (event) => {
+    
+  this.setState({ searchfield: event.target.value})
+}
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());     
+        })
+
+        if (!robots.length){
+            return <h1> Loading...</h1>
+        }
+        else{
+    return (
+        <div className='tc'>
+         <h1 className='f1'> ROBOTFRIENDS </h1>
+            <SearchBox searchchange = {this.onSearchChange} />
+            <Scroll>
+                <CardList  robots={filterRobots}/>
+            </Scroll>
+            
+        </div>
+        
+    );
+        }
+}
+}
+
+export default App;
